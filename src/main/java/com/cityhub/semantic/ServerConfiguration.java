6099e4f126ea6e24f929aab4e0706d89c6682838
jieun <love9ly@gmail.com>
2020-09-17 18:15:46 +0900

    configuration updated with 8082

--- src/main/java/com/cityhub/semantic/ServerConfiguration.java
+++ src/main/java/com/cityhub/semantic/ServerConfiguration.java
@@ -2,16 +2,16 @@ package com.cityhub.semantic;
 
 public class ServerConfiguration {
 
-	public static final String API_BASE_URI = "semantic/api/v1";
+	public static final String API_BASE_URI = "";
 	
-	public static final String TDB_BASE_URL = "jdbc:virtuoso://localhost:1111";
-	//public static final String TDB_BASE_URL = "jdbc:virtuoso://172.20.0.118:1111";
+//	public static final String TDB_BASE_URL = "jdbc:virtuoso://localhost:1111";
+	public static final String TDB_BASE_URL = "jdbc:virtuoso://172.20.0.129:1111";
 	
-	public static final String TDB_USER = "exUser";
-	//public static final String TDB_USER = "dba";
+//	public static final String TDB_USER = "exUser";
+	public static final String TDB_USER = "dba";
 	
-	public static final String TDB_PASS = "keti123";
-	//public static final String TDB_PASS = "dba";
+//	public static final String TDB_PASS = "keti123";
+	public static final String TDB_PASS = "dba";
 	
 	public static final String MAX_ONTOLOGY_LENGTH = "80";
 }
